64e6379b13cbca9cbdf32d6c3320111710080e49
Iury Seredenko <iury.seredenko@gmail.com>
2024-04-30 14:44:34 +0300

    Restarted watching the seminar, extratrain

---
+++ Python_Course/Seminar01/Task00.py
@@ -0,0 +1,21 @@
+#====================================================
+print()
+print("Developer - Python_Course, Seminar_01")
+print("Seredenko I.D.")
+print("Full Tasks in one file.")
+
+# print("Task 01") #================================================================================================ Task 01
+# print()
+# # The car ride n km per day
+# # How much day the car will spend to ride the trip with m km?
+# # diring the task you should not use IF or Cikles operators
+
+# n = int(input ("Add how much kilometers the car can ride per day and press ENTER:  "))
+# m = int(input("Add the Trip Distance in kilometers and press ENTER:  "))
+
+# print("Car can ride per day ", n, " km.")
+# print("The ride trip distance is  ", m, " km.")
+
+# print("The car will spend ", (m + n - 1)//n, " days for the road")
+
+print("Task 02") #================================================================================================ Task 02
